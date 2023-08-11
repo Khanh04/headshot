942a97a37d99b46cc399b34a30cebed07a67c971
Khanh Nguyen <tsukumiyo@gmail.com>
2023-08-11 18:16:48 +0300

    allow setting background color

--- app.py
+++ app.py
@@ -15,8 +15,11 @@ def cv2base64(image_array):
     processed_img_base64 = base64.b64encode(buffer).decode("utf-8")
     return processed_img_base64
 
+
 class ImageInput(BaseModel):
     image_data: str
+    background_color: str = "w"
+
 
 class ImageOutput(BaseModel):
     image: str
@@ -27,17 +30,14 @@ class ImageOutput(BaseModel):
 async def upload_file(payload: ImageInput):
     # Read the image using cv2
     image_bytes = base64.b64decode(payload.image_data)
-    
+
     # Convert bytes to numpy array
     image_np = np.frombuffer(image_bytes, np.uint8)
 
     image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
 
     # Process the image
-    image_rgba, mask = remove_background(image)
-
-    # Swap the Red and Blue channels in the RGBA image
-    image_rgba[:, :, [0, 2]] = image_rgba[:, :, [2, 0]]
+    image_rgba, mask = remove_background(image, payload.background_color)
 
     return ImageOutput(image=cv2base64(image_rgba), mask=cv2base64(mask))
 
