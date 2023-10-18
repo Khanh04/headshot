604a07c8ed5fbc2cfac4eda354f24dec68e4261a
Khanh Nguyen <tsukumiyo@gmail.com>
2023-10-18 09:02:34 +0300

    add new endpoint for fetching and downloading available background images

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+import os
+
 import uvicorn
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from models import ImageInput, ImageOutput
 from portrait_seg import remove_background
+from setting import BACKGROUND_FOLDER
 from utils import cv2base64
 
 app = FastAPI()
@@ -14,6 +18,18 @@ async def upload_file(payload: ImageInput) -> ImageOutput:
     image_rgba, mask = remove_background(payload.image, payload.background_color)
 
     return ImageOutput(image=cv2base64(image_rgba), mask=cv2base64(mask))
+
+
+@app.get("/backgrounds", response_model=list[dict])
+async def list_backgrounds():
+    """
+    List all available backgrounds
+    """
+    backgrounds = os.listdir(BACKGROUND_FOLDER)
+    return [{"id": idx, "name": bg} for idx, bg in enumerate(backgrounds)]
+
+
+app.mount("/static", StaticFiles(directory="backgrounds"), name="static")
 
 
 if __name__ == "__main__":
